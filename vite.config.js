cbb7dd70b7ba10a4e021fc39d82fbca8bbe73d3f
bloominolive <bloomin.olive88@gmail.com>
2023-07-14 21:21:55 -0400

    fixes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,16 @@
-// module.exports = {
-//     build: {
-//       outDir: "dist",
-//       base: "/flapmap/",
-//     },
-//   };
-  import { resolve } from "path";
-  import { defineConfig } from "vite";
-  
-  export default defineConfig({
-    root: "src/",
-  
+import { resolve } from "path";
+module.exports = {
     build: {
-      outDir: "../dist",
+      outDir: "dist",
+      base: "/flapmap/",
       rollupOptions: {
         input: {
           main: resolve(__dirname, "src/index.html"),
           birdsList: resolve(__dirname, "src/birdsList.html"),
           birdsJournal: resolve(__dirname, "src/birdsJournal.html"),
-        },
-      },
     },
-  });
+  }
+},
+};
+  
+ 
